9d544252543d5c3050416d58cfcad93da170decb
zyrooi <kietblox7777@gmail.com>
2025-03-19 16:34:56 +0700

    Create KAISER-KID.lua

---
+++ KAISER-KID.lua
@@ -0,0 +1,5 @@
+if game.GameId == 994732206 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrooi/zyroscript/refs/heads/main/Not-premium.lua"))()
+elseif game.GameId == 1451439645 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrooi/zyroscript/refs/heads/main/kinglegacy.lua"))()
+end
