ca62aa5450d7a412f97ba89c71ba90b40153b4ab
zyrooi <kietblox7777@gmail.com>
2025-03-20 13:49:09 +0700

    Update KAISER-KID.lua

--- KAISER-KID.lua
+++ KAISER-KID.lua
@@ -2,4 +2,5 @@ if game.GameId == 994732206 then -- Blox Fruit
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrooi/zyroscript/refs/heads/main/Not-premium.lua"))()
 elseif game.GameId == 1451439645 then -- King Legacy 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrooi/zyroscript/refs/heads/main/kinglegacy.lua"))()
+elseif game.GameId == 3756501373 then - Meme Sea
 end
